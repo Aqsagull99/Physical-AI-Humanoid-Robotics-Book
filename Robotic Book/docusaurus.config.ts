01805a00e86c24c97d5420ea920f2be69454901d
Aqsa Gull <159878357+Aqsagull99@users.noreply.github.com>
2025-12-10 11:32:13 +0500

    Update production URL for Docusaurus site

--- Robotic Book/docusaurus.config.ts
+++ Robotic Book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-humanoid-robotics-book-fxeu-8l3ik8ff5.vercel.app/',
+  url: 'https://aqsagull99.github.io/Physical-AI-Humanoid-Robotics-Book/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
