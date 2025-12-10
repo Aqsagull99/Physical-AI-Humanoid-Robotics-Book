a488d6e9c3c882acfa898409838dc70d5d2b58ef
Aqsa Gull <159878357+Aqsagull99@users.noreply.github.com>
2025-12-10 11:48:57 +0500

    Update baseUrl for GitHub Pages deployment

--- Robotic Book/docusaurus.config.ts
+++ Robotic Book/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://aqsagull99.github.io/Physical-AI-Humanoid-Robotics-Book/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Physical-AI-Humanoid-Robotics-Book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
